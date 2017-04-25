b0fd272450ace6ff4c5b76c45dd6489ebce905bb
OMarciano <lmevstarts@gmail.com>
2017-04-25 03:26:26 -0300

    hehe

--- match.php
+++ match.php
@@ -229,7 +229,7 @@
                 <center><h1>COMPARANDO MATCHS</h1></center>
                 <?php 
                 sleep(2);
-                header("Location:https://evstarts.com/matchinvestidor.php ");
+                echo '<meta http-equiv="refresh" content="1; URL=matchinvest.php'>";
  ?>
             <!--rightside bar -->
             <div id="right">
