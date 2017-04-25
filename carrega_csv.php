ec36abb4343f2f8e4eb2f98309b6d36a66d75412
Pedro <cenci.br@gmail.com>
2017-04-24 22:39:18 -0300

    add

--- carrega_csv.php
+++ carrega_csv.php
@@ -17,15 +17,29 @@
 
 	$row = 1;
 	if (($handle = fopen("EmpresasIncentivo.csv", "r")) !== FALSE) {
-    	while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        	$num = count($data);
-        	echo "<p> $num campos na linha $row: <br /></p>\n";
-        	$row++;
-        	for ($c=0; $c < $num; $c++) {
-            	echo $data[$c] . "<br />\n";
-        }
-    }
+    	while (sizeof($row) < $data)
+    		while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        			echo <tr>$row
+        					<td>$data
+        				</tr>	
+
+        				 
+
+
+
+        			     \n;
+        			$data++;
+        			for ($c=0; $c < $num; $c++) {
+            			echo $data[$c] . "<br />\n";
+        	}
+    	
+    	}
+    }	
+
+
     fclose($handle);
+
+
 }
 ?>
 
